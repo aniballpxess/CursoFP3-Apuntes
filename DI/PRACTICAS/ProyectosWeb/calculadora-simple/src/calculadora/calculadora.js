5f82bb4e6b490c93e2d2aab70fe97adf4e6471e6
aniballpxess <aniballpxess@gmail.com>
2026-01-08 19:41:53 +0100

    Actualizada interfáz del proyecto "calculadora-simple" de DI
    HTML:
     - Elimidados placeholders para testear la visualización de la interfáz
     - Renombramientos de varios ids para una meyor claridad
     - Eliminado almacenaje externo de valores asociados con botones (ahora se guarda en una enum interna consultable)
    
    CSS:
     - Cambios de estilo para mejorar visualización en los displays y para aumentar la rigidez de la interfaz ante overflows de texto en los susodichos
     - Ampliada clase "clickable" para arreglar inconsistencia visual al pulsar botones desactivados
    
    JS:
     - Renombramiento y reordenamiento de variables referenciales a elementos externos (HTML) y de miembros de enums y de las variables internas  del autómata para una mayor claridad
     - Añadidas variables internas para gestionar ejecución de las distintas operaciones requeridas (+, -, x, /)
     - Añadidos enums para internalizar el almacenamiento de los valores asociados con los botones de la GUI
     - Modificado el control lógico de lo que se muestra en los displays (siendo removido dicho control de la función "updateExternals")
     - Relegada la función "updateInternals" a la sección de "TESTING" y sustituida por la función "runAction"
     - Totalmente desarrollada la función "runAction" incluyendo:
        - La ejecucíon de las operaciones de cálculo (+, -, x, /)
        - La creación de registros de dichas operaciones
        - La gestión de la edición de los números mediante los 3 métodos disponibles (type, delete, clear)
        - El reinicio total del autómata al estado por defecto

--- DI/PRACTICAS/ProyectosWeb/calculadora-simple/src/calculadora/calculadora.js
+++ DI/PRACTICAS/ProyectosWeb/calculadora-simple/src/calculadora/calculadora.js
@@ -3,22 +3,26 @@
 
 // --- UI ELEMENTS ---
 
+// Viewers
+/** @type {HTMLDivElement} */
+const history_viewer = document.getElementById('history-view');
+
 // Displays
 /** @type {HTMLSpanElement} */
-const display_operation = document.getElementById('display-operation');
+const operation_display = document.getElementById('operation-display');
 /** @type {HTMLSpanElement} */
-const display_number = document.getElementById('display-number');
+const number_display = document.getElementById('number-display');
 
 // Action buttons
 /** @type {HTMLButtonElement} */
-const action_btn_cln = document.getElementById('action-btn-cln');
+const action_btn_clr = document.getElementById('action-btn-clr');
 /** @type {HTMLButtonElement} */
-const action_btn_rst = document.getElementById('action-btn-reset');
+const action_btn_rst = document.getElementById('action-btn-rst');
 /** @type {HTMLButtonElement} */
-const action_btn_del = document.getElementById('action-btn-delete');
-/** @type {HTMLButtonElement} */
-const action_btn_eql = document.getElementById('action-btn-equal');
+const action_btn_del = document.getElementById('action-btn-del');
 
+/** @type {HTMLButtonElement} */
+const action_btn_eql = document.getElementById('action-btn-eql');
 /** @type {HTMLButtonElement} */
 const action_btn_div = document.getElementById('action-btn-div');
 /** @type {HTMLButtonElement} */
@@ -30,7 +34,7 @@ const action_btn_add = document.getElementById('action-btn-add');
 
 // Typing buttons
 /** @type {HTMLButtonElement} */
-const typing_btn_dot = document.getElementById('typing-btn-dot');
+const typing_btn_dot = document.getElementById('typing-btn-d');
 /** @type {HTMLButtonElement} */
 const typing_btn_0 = document.getElementById('typing-btn-0');
 /** @type {HTMLButtonElement} */
@@ -67,36 +71,71 @@ const STATE = {
 };
 /** @enum {string} */
 const ACTION = {
-    CLEAR_DISPLAY: 'CLEAR_DISPLAY',
-    RESET_SYSTEM: 'RESET_SYSTEM',
-    DELETE_CHARACTER: 'DELETE_CHARACTER',
-    SELECT_OPERATION: 'SELECT_OPERATION',
     TYPE_CHARACTER: 'TYPE_CHARACTER',
-    FINISH_CALCULI: 'FINISH_CALCULI',
+    RESET_SYSTEM: 'RESET_SYSTEM',
+    CLEAR_NUMBER_DISPLAY: 'CLEAR_NUMBER',
+    DELETE_CHARACTER: 'DELETE_CHARACTER',
+    SELECT_OPERAND: 'SELECT_OPERAND',
+    FINISH_OPERATIONS: 'FINISH_OPERATIONS',
 };
 /** @enum {string} */
 const EVENT = {
     CLICK: 'click',
     CHANGE: 'change',
 };
+/** @enum {string} */
+const CHARACTER = {
+    ONE: '1',
+    TWO: '2',
+    THREE: '3',
+    FOUR: '4',
+    FIVE: '5',
+    SIX: '6',
+    SEVEN: '7',
+    EIGHT: '8',
+    NINE: '9',
+    ZERO: '0',
+    DOT: '.',
+};
+/** @enum {string} */
+const OPERAND = {
+    ADDITION: '+',
+    SUBTRACTION: '-',
+    PRODUCT: 'x',
+    DIVISION: '/',
+    EQUAL: '=',
+};
 /** @type {STATE} */
 const DEFAULT_STATE = STATE.FIRST_CLEAR;
 
 // Variables
 /** @type {STATE} */
-let current_state = DEFAULT_STATE;
+let current_state = null;
 /** @type {ACTION} */
 let current_action = null;
 /** @type {HTMLInputElement | HTMLButtonElement} */
 let current_trigger = null;
-/** @type {string} */
-let displayed_number = '';
-/** @type {string} */
-let displayed_operation = '';
+
 /** @type {boolean} */
 let wrong_trigger = false;
 /** @type {boolean} */
 let wrong_action = false;
+
+/** @type {OPERAND} */
+let current_operand = null;
+/** @type {OPERAND} */
+let next_operand = null;
+/** @type {number} */
+let first_number = 0;
+/** @type {number} */
+let second_number = 0;
+/** @type {number} */
+let result = 0;
+
+/** @type {string} */
+let displayed_number = '';
+/** @type {string} */
+let displayed_operation = '';
 
 // Functions
 /**
@@ -107,12 +146,170 @@ function eventsHandler(action, trigger) {
     current_action = action;
     current_trigger = trigger;
 
+    runAction();
     updateState();
-    updateInternals(action, trigger);
     updateExternals();
 
     current_action = null;
     current_trigger = null;
+}
+
+/**
+ *
+ */
+function runAction() {
+    switch (current_action) {
+        case ACTION.TYPE_CHARACTER:
+            printCharacter();
+            break;
+
+        case ACTION.RESET_SYSTEM:
+            first_number = null;
+            second_number = null;
+            result = null;
+            current_operand = null;
+            next_operand = null;
+            history_viewer.replaceChildren();
+            displayed_number = '';
+            displayed_operation = '';
+            break;
+
+        case ACTION.CLEAR_NUMBER_DISPLAY:
+            displayed_number = '';
+            break;
+
+        case ACTION.DELETE_CHARACTER:
+            displayed_number = displayed_number.slice(0, -1);
+            break;
+
+        case ACTION.SELECT_OPERAND:
+            getNextOperand();
+            doCurrentOperation();
+            recordOperation();
+            displayed_operation = `${result} ${next_operand}`;
+            displayed_number = '';
+            break;
+
+        case ACTION.FINISH_OPERATIONS:
+            getNextOperand();
+            doCurrentOperation();
+            recordOperation();
+            displayed_operation = `${first_number} ${current_operand} ${second_number} =`;
+            displayed_number = result;
+            break;
+
+        default:
+            break;
+    }
+}
+
+/**
+ *
+ */
+function printCharacter() {
+    let typed_character;
+    switch (current_trigger) {
+        case typing_btn_0:
+            typed_character = CHARACTER.ZERO;
+            break;
+        case typing_btn_1:
+            typed_character = CHARACTER.ONE;
+            break;
+        case typing_btn_2:
+            typed_character = CHARACTER.TWO;
+            break;
+        case typing_btn_3:
+            typed_character = CHARACTER.THREE;
+            break;
+        case typing_btn_4:
+            typed_character = CHARACTER.FOUR;
+            break;
+        case typing_btn_5:
+            typed_character = CHARACTER.FIVE;
+            break;
+        case typing_btn_6:
+            typed_character = CHARACTER.SIX;
+            break;
+        case typing_btn_7:
+            typed_character = CHARACTER.SEVEN;
+            break;
+        case typing_btn_8:
+            typed_character = CHARACTER.EIGHT;
+            break;
+        case typing_btn_9:
+            typed_character = CHARACTER.NINE;
+            break;
+        case typing_btn_dot:
+            typed_character = CHARACTER.DOT;
+            break;
+        default:
+            typed_character = '';
+            break;
+    }
+    displayed_number = displayed_number + typed_character;
+}
+
+/**
+ *
+ */
+function getNextOperand() {
+    current_operand = next_operand;
+    switch (current_trigger) {
+        case action_btn_add:
+            next_operand = OPERAND.ADDITION;
+            break;
+        case action_btn_sub:
+            next_operand = OPERAND.SUBTRACTION;
+            break;
+        case action_btn_prd:
+            next_operand = OPERAND.PRODUCT;
+            break;
+        case action_btn_div:
+            next_operand = OPERAND.DIVISION;
+            break;
+        case action_btn_eql:
+            next_operand = OPERAND.EQUAL;
+            break;
+    }
+}
+
+/**
+ *
+ */
+function doCurrentOperation() {
+    if (current_operand === null) {
+        result = Number.parseFloat(displayed_number);
+        return;
+    }
+    first_number = result;
+    second_number = Number.parseFloat(displayed_number);
+    switch (current_operand) {
+        case OPERAND.ADDITION:
+            result = first_number + second_number;
+            break;
+        case OPERAND.SUBTRACTION:
+            result = first_number - second_number;
+            break;
+        case OPERAND.PRODUCT:
+            result = first_number * second_number;
+            break;
+        case OPERAND.DIVISION:
+            result = first_number / second_number;
+            break;
+    }
+}
+
+/**
+ *
+ */
+function recordOperation() {
+    if (current_operand === null) {
+        return;
+    }
+    const record = document.createElement('div');
+    record.classList.add('record');
+    record.textContent = `${first_number} ${current_operand} ${second_number} = ${result}`;
+    history_viewer.appendChild(record);
 }
 
 /**
@@ -132,7 +329,7 @@ function updateState() {
                 current_state = STATE.FIRST_CLEAR;
                 break;
             }
-            if (current_action === ACTION.CLEAR_DISPLAY) {
+            if (current_action === ACTION.CLEAR_NUMBER_DISPLAY) {
                 current_state = STATE.FIRST_CLEAR;
                 break;
             }
@@ -142,7 +339,7 @@ function updateState() {
                 }
                 break;
             }
-            if (current_action === ACTION.SELECT_OPERATION) {
+            if (current_action === ACTION.SELECT_OPERAND) {
                 current_state = STATE.NEXT_CLEAR;
                 break;
             }
@@ -164,7 +361,7 @@ function updateState() {
                 current_state = STATE.FIRST_CLEAR;
                 break;
             }
-            if (current_action === ACTION.CLEAR_DISPLAY) {
+            if (current_action === ACTION.CLEAR_NUMBER_DISPLAY) {
                 current_state = STATE.NEXT_CLEAR;
                 break;
             }
@@ -174,11 +371,11 @@ function updateState() {
                 }
                 break;
             }
-            if (current_action === ACTION.SELECT_OPERATION) {
+            if (current_action === ACTION.SELECT_OPERAND) {
                 current_state = STATE.NEXT_CLEAR;
                 break;
             }
-            if (current_action === ACTION.FINISH_CALCULI) {
+            if (current_action === ACTION.FINISH_OPERATIONS) {
                 current_state = STATE.FINISHED;
                 break;
             }
@@ -194,58 +391,15 @@ function updateState() {
 }
 
 /**
- * @param {ACTION} action - the action to be executed by the event
- * @param {HTMLInputElement | HTMLButtonElement} trigger - the trigger that activated the event
- */
-function updateInternals(action, trigger) {
-    if (wrong_trigger) {
-        return;
-    }
-    if (wrong_action) {
-        return;
-    }
-
-    switch (current_state) {
-        case STATE.FIRST_CLEAR:
-            displayed_number = '';
-            displayed_operation = '';
-            break;
-        case STATE.FIRST_TYPING:
-            displayed_number = display_number;
-            displayed_operation = '';
-            break;
-        case STATE.NEXT_CLEAR:
-            displayed_number = '';
-            displayed_operation = display_operation;
-            break;
-        case STATE.NEXT_TYPING:
-            displayed_number = display_number;
-            displayed_operation = display_operation;
-            break;
-        case STATE.FINISHED:
-            displayed_number = display_number;
-            displayed_operation = display_operation;
-            break;
-    }
-}
-
-/**
  *
  */
 function updateExternals() {
-    if (wrong_trigger) {
-        return;
-    }
-    if (wrong_action) {
-        return;
-    }
-
     switch (current_state) {
         case STATE.FIRST_CLEAR:
-            display_number.textContent = '';
-            display_operation.textContent = '';
+            operation_display.textContent = displayed_operation;
+            number_display.textContent = displayed_number;
             action_btn_rst.disabled = true;
-            action_btn_cln.disabled = true;
+            action_btn_clr.disabled = true;
             action_btn_del.disabled = true;
             action_btn_eql.disabled = true;
             typing_btn_dot.disabled = false;
@@ -266,10 +420,10 @@ function updateExternals() {
             break;
 
         case STATE.FIRST_TYPING:
-            display_number.textContent = displayed_number;
-            display_operation.textContent = '';
+            operation_display.textContent = displayed_number;
+            number_display.textContent = displayed_number;
             action_btn_rst.disabled = false;
-            action_btn_cln.disabled = false;
+            action_btn_clr.disabled = false;
             action_btn_del.disabled = false;
             action_btn_eql.disabled = true;
             typing_btn_dot.disabled = false;
@@ -290,10 +444,10 @@ function updateExternals() {
             break;
 
         case STATE.NEXT_CLEAR:
-            display_number.textContent = '';
-            display_operation.textContent = displayed_operation;
+            operation_display.textContent = displayed_operation;
+            number_display.textContent = displayed_number;
             action_btn_rst.disabled = false;
-            action_btn_cln.disabled = true;
+            action_btn_clr.disabled = true;
             action_btn_del.disabled = true;
             action_btn_eql.disabled = true;
             typing_btn_dot.disabled = false;
@@ -314,10 +468,10 @@ function updateExternals() {
             break;
 
         case STATE.NEXT_TYPING:
-            display_number.textContent = displayed_number;
-            display_operation.textContent = displayed_operation;
+            operation_display.textContent = displayed_operation;
+            number_display.textContent = displayed_number;
             action_btn_rst.disabled = false;
-            action_btn_cln.disabled = false;
+            action_btn_clr.disabled = false;
             action_btn_del.disabled = false;
             action_btn_eql.disabled = false;
             typing_btn_dot.disabled = false;
@@ -338,10 +492,10 @@ function updateExternals() {
             break;
 
         case STATE.FINISHED:
-            display_number.textContent = displayed_number;
-            display_operation.textContent = displayed_operation;
+            operation_display.textContent = displayed_operation;
+            number_display.textContent = displayed_number;
             action_btn_rst.disabled = false;
-            action_btn_cln.disabled = true;
+            action_btn_clr.disabled = true;
             action_btn_del.disabled = true;
             action_btn_eql.disabled = true;
             typing_btn_dot.disabled = true;
@@ -363,12 +517,6 @@ function updateExternals() {
     }
 }
 
-/**
- *
- * @param {HTMLInputElement | HTMLButtonElement} trigger - the trigger that activated the event
- */
-function printChar(trigger) {}
-
 // -------------------------------------------------------------------------- //
 
 // --- EVENT HANDLING ---
@@ -387,19 +535,40 @@ typing_btn_9.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARA
 typing_btn_dot.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_dot));
 
 // Action buttons
-action_btn_cln.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.CLEAR_DISPLAY, action_btn_cln));
-action_btn_eql.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.RESET_SYSTEM, action_btn_eql));
+action_btn_clr.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.CLEAR_NUMBER_DISPLAY, action_btn_clr));
+action_btn_eql.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.FINISH_OPERATIONS, action_btn_eql));
 action_btn_rst.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.RESET_SYSTEM, action_btn_rst));
 action_btn_del.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.DELETE_CHARACTER, action_btn_del));
 
-action_btn_div.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.SELECT_OPERATION, action_btn_div));
-action_btn_prd.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.SELECT_OPERATION, action_btn_prd));
-action_btn_sub.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.SELECT_OPERATION, action_btn_sub));
-action_btn_add.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.SELECT_OPERATION, action_btn_add));
+action_btn_div.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.SELECT_OPERAND, action_btn_div));
+action_btn_prd.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.SELECT_OPERAND, action_btn_prd));
+action_btn_sub.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.SELECT_OPERAND, action_btn_sub));
+action_btn_add.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.SELECT_OPERAND, action_btn_add));
 
 // -------------------------------------------------------------------------- //
 
 // --- INCIALIZE INTERFACE ---
+current_state = DEFAULT_STATE;
 updateExternals();
 
 // -------------------------------------------------------------------------- //
+
+// --- TESTING ---
+
+/**
+ * TODO - See if needed at all
+ */
+function updateInternals() {
+    switch (current_state) {
+        case STATE.FIRST_CLEAR:
+            break;
+        case STATE.FIRST_TYPING:
+            break;
+        case STATE.NEXT_CLEAR:
+            break;
+        case STATE.NEXT_TYPING:
+            break;
+        case STATE.FINISHED:
+            break;
+    }
+}
