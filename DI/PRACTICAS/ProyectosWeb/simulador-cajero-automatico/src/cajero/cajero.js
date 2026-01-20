b6863f59eb4b144422d1c79db2a5b509a5e43c91
aniballpxess <aniballpxess@gmail.com>
2026-01-20 17:05:31 +0100

    Aztualizado proyecto "simulador-cajero-automatico" de DI
    CAMBIOS
     * HTML
        - Eliminados algunos sustitutos antiguos para poder probar la interfáz antes de la implementación de la lógica
        - Añadidas las ayudas visuales para los botones del display "CS", "SD" y "S"
     * JS
        - Modificado "printCharacter()" a "getDigit()" (incluyendo logica interna y variables y asociadas) para una mejor adaptación al nuevo Autómata
        - Añadidas variables internas "balance" y "withdrawal_amount" para gestionar la lógica de la simulación de una cuenta de la que se extrae dinero
        - Añadida variable interna "displayed_text" para gestionar la diferente información principal mostrada por pantalla en todo momento
        - Reordenada la inicialización de los EventListeners de una forma más coherente
        - Sustituidos los comentarios en "updateInternals()" por lineas en blanco para una mejor lectura a la par que se mantiene el orden previamente estructurado
        - Colocados los sustitutos de las funciones que sirvan para el registro de acciones y la consulta de dichos registros
        - Planteado el uso de una futura función "updateInternals()" con un proposito similar a "updateExternals()" pero para las variables internas
    PENDIENTE
     - Plantear una versión dinámica de las ayudas visuales de los botones del display "CS", "SD" y "S"
     - Completar la funcionalidad entorno al texto del display
     - Completar las funcionalidades entorno al registro y consulta de acciones realizadas

--- DI/PRACTICAS/ProyectosWeb/simulador-cajero-automatico/src/cajero/cajero.js
+++ DI/PRACTICAS/ProyectosWeb/simulador-cajero-automatico/src/cajero/cajero.js
@@ -73,7 +73,7 @@ const ACTION = {
     CHECK_BALANCE: 'CHECK_BALANCE',
     TAKE_OUT_MONEY: 'TAKE_OUT_MONEY',
     EXIT: 'EXIT',
-    TYPE_CHARACTER: 'TYPE_CHARACTER',
+    TYPE_DIGIT: 'TYPE_DIGIT',
     CONFIRM_AMOUNT: 'CONFIRM_AMOUNT',
     WITHDRAW_MONEY: 'WITHDRAW_MONEY',
 };
@@ -83,17 +83,17 @@ const EVENT = {
     CHANGE: 'change',
 };
 /** @enum {string} */
-const CHARACTER = {
-    ONE: '1',
-    TWO: '2',
-    THREE: '3',
-    FOUR: '4',
-    FIVE: '5',
-    SIX: '6',
-    SEVEN: '7',
-    EIGHT: '8',
-    NINE: '9',
-    ZERO: '0',
+const DIGIT = {
+    ONE: 1,
+    TWO: 2,
+    THREE: 3,
+    FOUR: 4,
+    FIVE: 5,
+    SIX: 6,
+    SEVEN: 7,
+    EIGHT: 8,
+    NINE: 9,
+    ZERO: 0,
 };
 /** @type {STATE} */
 const DEFAULT_STATE = STATE.AWAIT_CARD_IN;
@@ -110,6 +110,13 @@ let current_trigger = null;
 let wrong_trigger = false;
 /** @type {boolean} */
 let wrong_action = false;
+
+/** @type {number} */
+let balance = 1000;
+/** @type {number} */
+let withdrawal_amount = 0;
+/** @type {string} */
+let displayed_text = '';
 
 // Functions
 /**
@@ -134,12 +141,14 @@ function eventsHandler(action, trigger) {
 function runAction() {
     switch (current_action) {
         case ACTION.OPEN_HISTORY:
+            openHistoryTab();
             break;
 
         case ACTION.CARD_ACTION:
             break;
 
         case ACTION.CHECK_BALANCE:
+            registerAction();
             break;
 
         case ACTION.TAKE_OUT_MONEY:
@@ -148,11 +157,13 @@ function runAction() {
         case ACTION.EXIT:
             break;
 
-        case ACTION.TYPE_CHARACTER:
-            printCharacter();
+        case ACTION.TYPE_DIGIT:
+            getDigit();
             break;
 
         case ACTION.CONFIRM_AMOUNT:
+            balance = balance - withdrawal_amount;
+            registerAction();
             break;
 
         case ACTION.WITHDRAW_MONEY:
@@ -160,45 +171,56 @@ function runAction() {
     }
 }
 
+function openHistoryTab() {
+    Window.prototype.document.open();
+}
+
+function registerAction() {
+    Window.prototype.localStorage.setItem();
+}
+
 /**
  *
  */
-function printCharacter() {
-    let typed_character;
+function getDigit() {
+    let typed_digit;
     switch (current_trigger) {
         case typing_btn_0:
-            typed_character = CHARACTER.ZERO;
+            typed_digit = DIGIT.ZERO;
             break;
         case typing_btn_1:
-            typed_character = CHARACTER.ONE;
+            typed_digit = DIGIT.ONE;
             break;
         case typing_btn_2:
-            typed_character = CHARACTER.TWO;
+            typed_digit = DIGIT.TWO;
             break;
         case typing_btn_3:
-            typed_character = CHARACTER.THREE;
+            typed_digit = DIGIT.THREE;
             break;
         case typing_btn_4:
-            typed_character = CHARACTER.FOUR;
+            typed_digit = DIGIT.FOUR;
             break;
         case typing_btn_5:
-            typed_character = CHARACTER.FIVE;
+            typed_digit = DIGIT.FIVE;
             break;
         case typing_btn_6:
-            typed_character = CHARACTER.SIX;
+            typed_digit = DIGIT.SIX;
             break;
         case typing_btn_7:
-            typed_character = CHARACTER.SEVEN;
+            typed_digit = DIGIT.SEVEN;
             break;
         case typing_btn_8:
-            typed_character = CHARACTER.EIGHT;
+            typed_digit = DIGIT.EIGHT;
             break;
         case typing_btn_9:
-            typed_character = CHARACTER.NINE;
+            typed_digit = DIGIT.NINE;
             break;
         default:
-            typed_character = '';
+            typed_digit = null;
             break;
+    }
+    if (typed_digit !== null) {
+        withdrawal_amount = withdrawal_amount * 10 + typed_digit;
     }
 }
 
@@ -267,19 +289,18 @@ function updateState() {
 function updateExternals() {
     switch (current_state) {
         case STATE.AWAIT_CARD_IN:
-            // Display parts
             label_check.hidden = true;
             label_take.hidden = true;
             label_exit.hidden = true;
-            main_text.textContent = '';
-            // Display buttons
+            main_text.textContent = displayed_text;
+
             act_btn_check.disabled = true;
             act_btn_take.disabled = true;
             act_btn_exit.disabled = true;
-            // Simulation buttons
+
             sim_btn_card.disabled = false;
             sim_btn_money.disabled = true;
-            // Numpad buttons
+
             act_btn_next.disabled = true;
             typing_btn_0.disabled = true;
             typing_btn_1.disabled = true;
@@ -291,24 +312,23 @@ function updateExternals() {
             typing_btn_7.disabled = true;
             typing_btn_8.disabled = true;
             typing_btn_9.disabled = true;
-            // History button
+
             act_btn_history.disabled = false;
             break;
 
         case STATE.SELECTING_OPERATION:
-            // Display parts
             label_check.hidden = false;
             label_take.hidden = false;
             label_exit.hidden = false;
-            main_text.textContent = '';
-            // Display buttons
+            main_text.textContent = displayed_text;
+
             act_btn_check.disabled = false;
             act_btn_take.disabled = false;
             act_btn_exit.disabled = false;
-            // Simulation buttons
+
             sim_btn_card.disabled = true;
             sim_btn_money.disabled = true;
-            // Numpad buttons
+
             act_btn_next.disabled = true;
             typing_btn_0.disabled = true;
             typing_btn_1.disabled = true;
@@ -320,24 +340,23 @@ function updateExternals() {
             typing_btn_7.disabled = true;
             typing_btn_8.disabled = true;
             typing_btn_9.disabled = true;
-            // History button
+
             act_btn_history.disabled = false;
             break;
 
         case STATE.CHECKING_BALANCE:
-            // Display parts
             label_check.hidden = true;
             label_take.hidden = true;
             label_exit.hidden = false;
-            main_text.textContent = '';
-            // Display buttons
+            main_text.textContent = displayed_text;
+
             act_btn_check.disabled = true;
             act_btn_take.disabled = true;
             act_btn_exit.disabled = false;
-            // Simulation buttons
+
             sim_btn_card.disabled = true;
             sim_btn_money.disabled = true;
-            // Numpad buttons
+
             act_btn_next.disabled = true;
             typing_btn_0.disabled = true;
             typing_btn_1.disabled = true;
@@ -349,24 +368,23 @@ function updateExternals() {
             typing_btn_7.disabled = true;
             typing_btn_8.disabled = true;
             typing_btn_9.disabled = true;
-            // History button
+
             act_btn_history.disabled = false;
             break;
 
         case STATE.TAKING_OUT_MONEY:
-            // Display parts
             label_check.hidden = true;
             label_take.hidden = true;
             label_exit.hidden = false;
-            main_text.textContent = '';
-            // Display buttons
+            main_text.textContent = displayed_text;
+
             act_btn_check.disabled = true;
             act_btn_take.disabled = true;
             act_btn_exit.disabled = false;
-            // Simulation buttons
+
             sim_btn_card.disabled = true;
             sim_btn_money.disabled = true;
-            // Numpad buttons
+
             act_btn_next.disabled = false;
             typing_btn_0.disabled = false;
             typing_btn_1.disabled = false;
@@ -378,24 +396,23 @@ function updateExternals() {
             typing_btn_7.disabled = false;
             typing_btn_8.disabled = false;
             typing_btn_9.disabled = false;
-            // History button
+
             act_btn_history.disabled = false;
             break;
 
         case STATE.AWAIT_MONEY_WITHDRAW:
-            // Display parts
             label_check.hidden = true;
             label_take.hidden = true;
             label_exit.hidden = true;
-            main_text.textContent = '';
-            // Display buttons
+            main_text.textContent = displayed_text;
+
             act_btn_check.disabled = true;
             act_btn_take.disabled = true;
             act_btn_exit.disabled = true;
-            // Simulation buttons
+
             sim_btn_card.disabled = true;
             sim_btn_money.disabled = false;
-            // Numpad buttons
+
             act_btn_next.disabled = true;
             typing_btn_0.disabled = true;
             typing_btn_1.disabled = true;
@@ -407,24 +424,23 @@ function updateExternals() {
             typing_btn_7.disabled = true;
             typing_btn_8.disabled = true;
             typing_btn_9.disabled = true;
-            // History button
+
             act_btn_history.disabled = false;
             break;
 
         case STATE.AWAIT_CARD_OUT:
-            // Display parts
             label_check.hidden = true;
             label_take.hidden = true;
             label_exit.hidden = true;
-            main_text.textContent = '';
-            // Display buttons
+            main_text.textContent = displayed_text;
+
             act_btn_check.disabled = true;
             act_btn_take.disabled = true;
             act_btn_exit.disabled = true;
-            // Simulation buttons
+
             sim_btn_card.disabled = false;
             sim_btn_money.disabled = true;
-            // Numpad buttons
+
             act_btn_next.disabled = true;
             typing_btn_0.disabled = true;
             typing_btn_1.disabled = true;
@@ -436,7 +452,7 @@ function updateExternals() {
             typing_btn_7.disabled = true;
             typing_btn_8.disabled = true;
             typing_btn_9.disabled = true;
-            // History button
+
             act_btn_history.disabled = false;
             break;
     }
@@ -446,28 +462,28 @@ function updateExternals() {
 
 // --- EVENT HANDLING ---
 
-// Numpad buttons
-act_btn_next.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.CONFIRM_AMOUNT, act_btn_next));
-typing_btn_0.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_0));
-typing_btn_1.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_1));
-typing_btn_2.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_2));
-typing_btn_3.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_3));
-typing_btn_4.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_4));
-typing_btn_5.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_5));
-typing_btn_6.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_6));
-typing_btn_7.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_7));
-typing_btn_8.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_8));
-typing_btn_9.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_CHARACTER, typing_btn_9));
+// Special buttons
+sim_btn_card.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.CARD_ACTION, sim_btn_card));
+sim_btn_money.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.WITHDRAW_MONEY, sim_btn_money));
+act_btn_history.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.O, act_btn_history));
 
 // Display buttons
 act_btn_check.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.CHECK_BALANCE, act_btn_check));
 act_btn_take.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TAKE_OUT_MONEY, act_btn_take));
 act_btn_exit.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.EXIT, act_btn_exit));
 
-// Special buttons
-sim_btn_card.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.CARD_ACTION, sim_btn_card));
-sim_btn_money.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.WITHDRAW_MONEY, sim_btn_money));
-act_btn_history.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.O, act_btn_history));
+// Numpad buttons
+act_btn_next.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.CONFIRM_AMOUNT, act_btn_next));
+typing_btn_0.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_0));
+typing_btn_1.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_1));
+typing_btn_2.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_2));
+typing_btn_3.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_3));
+typing_btn_4.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_4));
+typing_btn_5.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_5));
+typing_btn_6.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_6));
+typing_btn_7.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_7));
+typing_btn_8.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_8));
+typing_btn_9.addEventListener(EVENT.CLICK, () => eventsHandler(ACTION.TYPE_DIGIT, typing_btn_9));
 
 // -------------------------------------------------------------------------- //
 
